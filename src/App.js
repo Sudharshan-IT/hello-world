c4e730fbfb372ff32e74473d7b1d65a026d3641d
Sudharshan <sudharshanjavaguide@gmail.com>
2022-07-07 15:45:00 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World Welcome To ReactJS MR.Sudharshan Yadav Nandyala.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          **************Learn React in The Official Website******
         </a>
       </header>
     </div>
